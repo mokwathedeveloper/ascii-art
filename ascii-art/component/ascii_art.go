309928b3d49c7831eb0e41a802287e254ba91bc1
mmoffat <mokwamoffat@yahoo.com>
2024-06-10 18:37:32 +0300

    > feat: Implement Output function for printing ASCII graphics
    - Add  function to print ASCII graphics for a given string using provided file contents and ASCII map
    - Handle edge cases such as empty strings and newline characters
    - Validate input string to ensure all characters are printable ASCII
    - Split input string by  and print the corresponding ASCII graphics line by line
    - Add  function to determine the correct ASCII graphic file based on command-line arguments, with improved argument validation and error messages
    
    These changes enable the program to generate and print ASCII art from various graphic files based on user input, with robust error handling and input validation.

--- ascii-art/component/ascii_art.go
+++ ascii-art/component/ascii_art.go
@@ -1,10 +1,17 @@
 package component
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 // ASCII constants
 const (
 	firstAscii = 32
 	lastAscii  = 127
 )
+
 // MakeAsciiGraphicsMap maps runes to their corresponding starting points in the ASCII graphic.
 func MakeAsciiGraphicsMap(fileContents []string) map[rune]int {
 	asciiMap := map[rune]int{}
@@ -42,4 +49,39 @@ func GetTheCorrectFile(args []string) string {
 		}
 	}
 	return "standard.txt"
+}
+
+// Output prints the ASCII graphics for a given string, using the file contents and ASCII map provided.
+func Output(str string, fileContents []string, asciiMap map[rune]int) {
+	if len(str) == 0 {
+		return
+	}
+	if str == "\\n" {
+		fmt.Println()
+		return
+	}
+
+	// Check for unprintable ASCII characters
+	for _, char := range str {
+		if char < rune(firstAscii) || char > rune(lastAscii) {
+			fmt.Println("Unprintable ASCII character(s) in input. To run the program type go run . <text(printable ascii)> [-flag] where flag can be -s or -t")
+			os.Exit(0)
+		}
+	}
+
+	strArr := strings.Split(str, "\\n")
+	for _, line := range strArr {
+		if line == "" || line == "\n" {
+			fmt.Println()
+			continue
+		}
+		for i := 0; i < 8; i++ {
+			for _, char := range line {
+				if ascii, ok := asciiMap[char]; ok {
+					fmt.Print(fileContents[ascii+i])
+				}
+			}
+			fmt.Println()
+		}
+	}
 }
