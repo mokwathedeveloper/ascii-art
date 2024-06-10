b79b9dbe1c3457af859cbf20acdcb7ac5838744b
mmoffat <mokwamoffat@yahoo.com>
2024-06-10 18:32:00 +0300

    feat: Implement GetTheCorrectFile function
    - Add  function to determine the ASCII graphic file based on command-line arguments
    - Handle cases with insufficient or excessive arguments and print appropriate messages
    - Return the correct file name based on specified flags (, , , , )
    
    This function ensures the correct ASCII graphic file is selected based on user input, providing flexibility and error handling for various argument cases.

--- ascii-art/component/ascii_art.go
+++ ascii-art/component/ascii_art.go
@@ -15,3 +15,31 @@ func MakeAsciiGraphicsMap(fileContents []string) map[rune]int {
 	}
 	return asciiMap
 }
+
+// GetTheCorrectFile determines which ASCII graphic file to use based on the flag passed.
+func GetTheCorrectFile(args []string) string {
+	if len(args) == 1 {
+		fmt.Println("Not enough arguments. To run the program type go run . <text> [-flag] where flag can be -s or -t")
+		os.Exit(0)
+	}
+
+	if len(args) > 3 {
+		fmt.Println("Too many arguments. To run the program type go run . <text> [-flag] where flag can be -s or -t")
+		os.Exit(0)
+	}
+
+	if len(args) == 3 {
+		switch args[2] {
+		case "-t", "-thinkertoy", "thinkertoy":
+			return "thinkertoy.txt"
+		case "-s", "-shadow":
+			return "shadow.txt"
+		case "-standard", "standard":
+			return "standard.txt"
+		default:
+			fmt.Println("Invalid flag. To run the program type go run . <text> [-flag] where flag can be -s or -t")
+			os.Exit(0)
+		}
+	}
+	return "standard.txt"
+}
