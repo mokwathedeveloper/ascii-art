a47a5f03e103e58eac13edd03e412d4cd8cf9db8
mmoffat <mokwamoffat@yahoo.com>
2024-06-29 14:15:36 +0300

    feat: handling //n /n t

--- component/ascii_art.go
+++ component/ascii_art.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
 // ASCII constants
@@ -58,29 +59,36 @@ func Output(str string, fileContents []string, asciiMap map[rune]int) {
 	if len(str) == 0 {
 		return
 	}
-	if str == "\\n" {
-		fmt.Println()
-		return
-	}
 
-	// Check for unprintable ASCII characters
-	for _, char := range str {
-		if char < rune(firstAscii) || char > rune(lastAscii) {
-			fmt.Println("Unprintable ASCII character(s) in input. To run the program type go run . <text(printable ascii)> [-flag] where flag can be -s or -t")
-			os.Exit(0)
-		}
-	}
+	// Replace escape sequences with their actual meanings
+	str = strings.ReplaceAll(str, "\\n", "\n")
+	str = strings.ReplaceAll(str, "\\t", "\t")
+	str = strings.ReplaceAll(str, "\\r", "\r")
 
-	strArr := strings.Split(str, "\\n")
-	for _, line := range strArr {
-		if line == "" || line == "\n" {
+	// Split the input into lines
+	lines := strings.Split(str, "\n")
+
+	for _, line := range lines {
+		if line == "" {
 			fmt.Println()
 			continue
 		}
 		for i := 0; i < 8; i++ {
 			for _, char := range line {
-				if ascii, ok := asciiMap[char]; ok {
-					fmt.Print(fileContents[ascii+i])
+				if char < rune(firstAscii) || char > rune(lastAscii) {
+					if unicode.IsSpace(char) {
+						// Handle spaces and other white spaces correctly
+						fmt.Print(strings.Repeat(" ", 8))
+					} else {
+						fmt.Println("Unprintable ASCII character(s) in input. To run the program type go run . <text(printable ascii)> [-flag] where flag can be -s or -t")
+						os.Exit(0)
+					}
+				} else {
+					if ascii, ok := asciiMap[char]; ok {
+						fmt.Print(fileContents[ascii+i])
+					} else {
+						fmt.Print(strings.Repeat(" ", 8)) // Handle characters not in the map with spaces
+					}
 				}
 			}
 			fmt.Println()
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"ascii-art/component"
-
 	"os"
 	"strings"
 )
